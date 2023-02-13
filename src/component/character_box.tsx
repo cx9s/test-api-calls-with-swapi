f7b6ace4fc3df7ed2ba957cad70d2266cb689646
cx9s <cx9918@gmail.com>
2023-02-13 23:59:31 +0000

    remove className

--- src/component/character_box.tsx
+++ src/component/character_box.tsx
@@ -4,7 +4,7 @@ import { Character } from "./character";
 const CharacterBox: React.FC<{ character: Character }> = ({ character }) => {
   const { name, height, gender, homeworld, url } = character;
   return (
-    <div className="App">
+    <div>
       <h2>{name}</h2>
       <p>Height: {height}</p>
       <p>Gender: {gender}</p>
