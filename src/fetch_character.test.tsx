f29cbf113369c6d9599a15bce8a3005fa4af0506
cx9s <cx9918@gmail.com>
2023-02-14 16:01:31 +0000

    simple test by real api

--- src/fetch_character.test.tsx
+++ src/fetch_character.test.tsx
@@ -3,7 +3,7 @@ import user from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import FetchCharacter from "./component/fetch_character";
 
-test("Given the required props, When the api is called, Then the text should be present", async () => {
+test("Given the required props, When the api is called, Then the text should be present and button should be disabled", async () => {
   render(<FetchCharacter apiUrl="https://swapi.dev/api/people/1" />);
 
   // ACT
