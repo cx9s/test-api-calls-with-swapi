d2c4584efad811c1642e4bba365d509e5fb27547
cx9s <cx9918@gmail.com>
2023-02-14 00:06:40 +0000

    change variable name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,8 +3,8 @@ import App from "./App";
 
 test("renders h1 element", () => {
   render(<App />);
-  const linkElement = screen.getByText(
+  const h1Element = screen.getByText(
     /Testing API calls in React with the SWAPI API/i
   );
-  expect(linkElement).toBeInTheDocument();
+  expect(h1Element).toBeInTheDocument();
 });
